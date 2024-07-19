6fb2d9e1484c36ec077171e56d74e9b486f990c5
Umesh-Shirsat <Umesh.Shirsat@winjit.com>
2024-07-19 11:15:58 +0530

    feat: Added actions

--- cypress/e2e/childTabs.cy.js
+++ cypress/e2e/childTabs.cy.js
@@ -3,19 +3,19 @@ describe("child", () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get("#opentab").invoke("removeAttr", "target").click();
 
-    cy.origin("https://www.qaclickacademy.com/", () => {
-      cy.get("#navbarSupportedContent a[href*='about']").click();
-    });
-  });
+    // cy.origin("https://www.qaclickacademy.com/", () => {
+    // cy.get("#navbarSupportedContent a[href*='about']").click();
+    // });
+    // });
 
-  it.only("child windows", () => {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get("#opentab").then((el) => {
-      const url = el.prop("href");
-      cy.visit(url);
-      cy.origin(url, () => {
-        cy.contains("About us").click();
-      });
-    });
+    // it.only("child windows", () => {
+    //   cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    //   cy.get("#opentab").then((el) => {
+    //     const url = el.prop("href");
+    //     cy.visit(url);
+    //     cy.origin(url, () => {
+    //       cy.contains("About us").click();
+    //     });
+    //   });
   });
 });
